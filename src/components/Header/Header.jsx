2c4986c9f5d1b0121e1ff1833d3a8a1975978edc
AlexeiTim <timashkov060896@gmail.com>
2022-12-09 15:25:49 +0300

    14. CSS modules

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import classes from './Header.module.css';
 
 const Header = () => {
   return (
-    <header className='header'>
+    <header className={classes.header}>
       <img src="https://avatars.mds.yandex.net/i?id=69efe936e34a03901774d5e4639c58f97a8f99c8-3872711-images-thumbs&n=13" alt="logo" />
     </header>
   )
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import classes from './Profile.module.css';
+
 
 const Profile = () => {
   return (
-    <div className='content'>
+    <div className={classes.content}>
       <div>
         <img src="https://avatars.mds.yandex.net/i?id=e89619a25af9b12c104907c3de2db26c-5694541-images-thumbs&n=13" alt="123" />
       </div>
@@ -16,13 +18,13 @@ const Profile = () => {
         </div>
       </div>
       <div>
-        <div>
+        <div className={classes.item}>
           post1
         </div>
-        <div>
+        <div className={classes.item}>
           post2
         </div>
-        <div>
+        <div className={classes.item}>
           post3
         </div>
       </div>
