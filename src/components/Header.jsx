0f6826509a3c0a06ced04e8ee096744fbf2750de
AlexeiTim <timashkov060896@gmail.com>
2022-12-09 13:30:57 +0300

    init:add start files

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <img src="https://avatars.mds.yandex.net/i?id=69efe936e34a03901774d5e4639c58f97a8f99c8-3872711-images-thumbs&n=13" alt="logo" />
+    </header>
+  )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className='content'>
+      <div>
+        <img src="https://avatars.mds.yandex.net/i?id=e89619a25af9b12c104907c3de2db26c-5694541-images-thumbs&n=13" alt="123" />
+      </div>
+      <div>
+        Avatar + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New Post
+        </div>
+      </div>
+      <div>
+        <div>
+          post1
+        </div>
+        <div>
+          post2
+        </div>
+        <div>
+          post3
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Profile;
